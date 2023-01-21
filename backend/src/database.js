c9522c32e24bb7b5a544e39bbbf0217a1e5f3807
Edison <edinson.quizhpe@unl.edu.ec>
2023-01-20 20:36:21 -0500

    update commands

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,18 +1,12 @@
 const mongoose=require('mongoose')
-const MongoClient = require('mongodb').MongoClient;
-const uri= 'mongodb+srv://userAdmin:userAdmin@smiledent.w8li3xp.mongodb.net/?retryWrites=true&w=majority';
-MongoClient.connect(uri, {useUnifiedTopology: true}, (err,client)=>{
-    if(err) return console.log(err);
-    console.log("Base de datos conectada correctamente");
-})
-//URI=('mongodb+srv://userAdmin:user@admin1234@smiledent.w8li3xp.mongodb.net/?retryWrites=true&w=majority')
+URI=('mongodb+srv://userAdmin:admin1234@smiledent.w8li3xp.mongodb.net/?retryWrites=true&w=majority')
 
-/*mongoose.connect(URI,{
+mongoose.connect(URI,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
     useCreateIndex:true,
     useFindAndModify:false  //cambiar a true si da error
 })
     .then(db=>console.log("Base de datos conectada correctamente"))
-    .catch(error=>console.log(error))*/
-module.exports=MongoClient
+    .catch(error=>console.log(error))
+module.exports=mongoose
